85e0271be86b83a7df27a8fa8cba87450d25a398
Md Abid Sikder <abid@abidsikder.com>
2022-08-23 11:40:33 -0400

    docs: add natspec documentation

--- src/LatLonDistance.sol
+++ src/LatLonDistance.sol
@@ -4,14 +4,23 @@ import {PRBMathSD59x18 as P} from "prb-math/PRBMathSD59x18.sol";
 import {Trigonometry as T} from "solidity-trigonometry/Trigonometry.sol";
 import {Arcsin as A} from "solidity-trigonometry/Arcsin.sol";
 
+/**
+ * @notice Calculates distance (in meters) between two locations by
+ * approximating the earth as a sphere and using the great sphere distance
+ * formula. See the python implementation on
+ * https://www.vibhuagrawal.com/blog/geospatial-nearest-neighbor-search.
+ *
+ * @author Md Abid Sikder
+ */
 library LatLonDistance {
   using P for int256;
 
-  // IUGG mean radius (R_1) = 6371008.7714 meters, with 18 decimal points to keep consistent with PRBMath
-  // DOI: 10.1007/BF02521480
+  /**
+     * @notice IUGG mean radius R_1 = 6371008.7714 meters, with 18 decimal
+     * points. See DOI: 10.1007/BF02521480 for the original paper source.
+     */
   int256 constant earthRadius = 6371008771400000000000000;
   
-  // with 18 decimal points
   int256 constant maxLat = 90000000000000000000;
   int256 constant minLat = -maxLat;
   int256 constant maxLon = 180000000000000000000;
@@ -29,32 +38,36 @@ library LatLonDistance {
     _;
   }
 
-  // mpd1 = mulPiDiv180
+  /**
+     * @dev Only for internal library use. Multiplies by pi and divides by 180.
+     */
   function mpd1(int256 _x) internal pure returns (int256) {
     return _x.mul(P.pi()).div(P.fromInt(180));
   }
 
-  // sin^2((lat2 - lat1)/2)
+  /**
+     * @notice Auxiliary function for use by library.
+     * @dev Calculates $\sin^2(\frac{lat_2 - lat_1}{2})$
+     */
   function aux1(int256 lat1, int256 lat2) internal pure returns (int256) {
     int256 TWO = P.fromInt(2);
 
-    int256 t3 = lat2 > lat1
-      ? lat2 - lat1
-      : lat1 - lat2;
+    int256 delta = lat2 - lat1;
 
-    // we are squaring the sine anyway so throw away the negative
-    int256 t4 = t3.div(TWO);
-    uint256 t5 = uint256(t4.abs());
-    int256 t1 = T.sin(t5);
+    uint256 sinArg = uint256(delta.div(TWO).abs());
+    int256 sinOut = T.sin(sinArg);
 
-    int256 sinSq1 = t1.mul(t1);
+    int256 sinSq = sinOut.mul(sinOut);
 
-    return sinSq1;
+    return sinSq;
   }
 
-  // cos(lat1) * cos(lat2)
+  /**
+     * @notice Auxiliary function for use by library.
+     * @dev Calculates $\cos(lat_1) \cdot \cos(lat_2)$
+     */
   function aux2(int256 lat1, int256 lat2) internal pure returns (int256) {
-    // cos(-x) = cos(x), so don't worry about negatives
+    // cos(-x) = cos(x), so taking the absolute value will not change the result
     uint256 l1 = uint256(lat1.abs());
     uint256 l2 = uint256(lat2.abs());
 
@@ -63,43 +76,61 @@ library LatLonDistance {
     return cosProd;
   }
 
-  // sin^2((lon2 - lon1)/2)
+  /**
+     * @notice Auxiliary function for use by library.
+     * @dev Calculates $\sin^2(\frac{lon_2 - lon_1}{2})$
+     */
   function aux3(int256 lon1, int256 lon2) internal pure returns (int256) {
     int256 TWO = P.fromInt(2);
-    
-    int256 t1 = lon2 > lon1
-      ? lon2 - lon1
-      : lon1 - lon2;
-    t1 = mpd1(t1);
 
-    t1 = t1.div(TWO).abs();
-    uint256 t3 = uint256(t1);
+    // Since we square the result of the sine, ignore the sign of the argument
+    int256 delta = (lon2 - lon1);
 
-    int256 t2 = T.sin(t3);
-    int256 sinSq2 = t2.mul(t2);
+    // take the absolute value before casting to uint256 to prevent issues with
+    // the first two's complement bit blowing up the size
+    uint256 sinArg = uint256(delta.div(TWO).abs());
 
-    return sinSq2;
+    int256 sinOut = T.sin(sinArg);
+    int256 sinSq = sinOut.mul(sinOut);
+
+    return sinSq;
   }
 
+  /**
+     * @notice Calculates distance between two latitude longitude points, by
+     * approximating the earth as a sphere and taking the shortest great circle
+     * distance.
+     *
+     * All parameters must be given in terms of degrees and as fixed-width
+     * decimal integers, with 18 decimal points.
+     *
+     * @param _lat1 Point 1 Latitude
+     * @param _lon1 Point 1 Longitude
+     * @param _lat2 Point 2 Latitude
+     * @param _lon2 Point 2 Longitude
+     *
+     * @return Distance as an integer with 18 fixed-width decimal points.
+     */
   function distance(int256 _lat1, int256 _lon1, int256 _lat2, int256 _lon2) internal pure 
   validLat(_lat1) validLat(_lat2) validLon(_lon1) validLon(_lon2)
   returns (int256) {
+    int256 TWO = P.fromInt(2);
+
+    // convert to radian measure
     _lat1 = mpd1(_lat1);
     _lat2 = mpd1(_lat2);
+    _lon1 = mpd1(_lon1);
+    _lon2 = mpd1(_lon2);
 
-    // sin^2((lat2 - lat1)/2)
+    // split up computation to avoid "Stack Too Deep" errors
     int256 a1 = aux1(_lat1, _lat2);
-
-    // cos(lat1) * cos(lat2)
     int256 a2 = aux2(_lat1, _lat2);
-
-    // sin^2((lon2 - lon1)/2)
     int256 a3 = aux3(_lon1, _lon2);
+
 
     int256 a4 = a1 + a2.mul(a3);
     int256 a5 = a4.sqrt();
 
-    int256 TWO = P.fromInt(2);
     int256 a6 = A.arcsin(a5);
 
     int256 result = a6.mul(earthRadius).mul(TWO);
